9ce73401ad49211302824c61203dbd6426e2cf38
TheSchoolFriday <20046797@myrp.edu.sg>
2021-05-07 17:21:33 +0800

    Completeed

--- app/src/main/java/sg/edu/rp/c346/id20046797/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046797/billplease/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Values are initiated inside the OnCreate Method, but just for this lesson.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
         // RadioGroup Group
         RadioGroup radioGroupPay = findViewById(R.id.RadioGroupPayment);
 
+        // RadioButton
+        RadioButton rbCash = findViewById(R.id.radioButtonCash);
+        RadioButton rbPaynow = findViewById(R.id.radioButtonPayNow);
+
+
         // ToggleButton Group
         ToggleButton tbSVS = findViewById(R.id.toggleButtonSVS);
         ToggleButton tbGST = findViewById(R.id.toggleButtonGST);
@@ -40,28 +48,72 @@ public class MainActivity extends AppCompatActivity {
         btnSplit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                String paymentType = "";
+
+                double discount = 0.0;
+                double surcharge = 1.0;
+                try {
+                    discount = Double.parseDouble(ETDiscount.getText().toString());
+                } catch (Exception E) {
+                    displayPrice.setText("Invalid Operator: " + E);
+                }
+
                 boolean SVS = false;
                 if(tbSVS.isChecked()) {
                     SVS = true;
+                    surcharge += 0.10;
                 }
 
                 boolean GST = false;
                 if(tbGST.isChecked()) {
                     GST = true;
+                    surcharge += 0.07;
                 }
 
-                double TotalAmount = Double.parseDouble(ETtotalAmount.getText().toString());
-                int TotalPax = Integer.parseInt(ETtotalPax.getText().toString());
-                double discount = Double.parseDouble(ETDiscount.getText().toString());
+                int paymentRadioCheck = radioGroupPay.getCheckedRadioButtonId();
+                if(paymentRadioCheck == R.id.radioButtonCash)  {
+                    paymentType = " in Cash";
+                } else {
+                    paymentType = " via PayNow to 9283 2392";
+                }
 
                 if (discount <= 100 && discount >= 0) {
+                    discount /= 100;
+                    try {
+                        double TotalAmount = Double.parseDouble(ETtotalAmount.getText().toString());
+                        int TotalPax = Integer.parseInt(ETtotalPax.getText().toString());
+
+                        double finalAmountAfterDisc = TotalAmount-(TotalAmount*discount);
+                        finalAmountAfterDisc = finalAmountAfterDisc * surcharge;
+                        double finalAmountAfterSplit = finalAmountAfterDisc / TotalPax;
+
+                        String FAAD = String.format("%.2f", finalAmountAfterDisc);
+                        String FAAS = String.format("%.2f", finalAmountAfterSplit);
+                        displayPrice.setText("Total Bill: $" + FAAD);
+                        displayType.setText("Each Bill: $" + FAAS + paymentType);
+                    } catch (Exception E){
+                        displayPrice.setText("Invalid Operator: " + E);
+                    }
 
                 } else {
-                    displayPrice.setText("You can't have discount more than 100% or lesser than 0%");
+                    displayPrice.setText("Can't have discount more than 100% or lesser than 0%");
                 }
 
-                // Check Payment Options
-//                int paymentRadioCheck = radioGroupPay.getCheckedRadioButtonId();
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayPrice.setText("");
+                displayType.setText("");
+                tbGST.setChecked(false);
+                tbSVS.setChecked(false);
+                ETtotalAmount.setText("");
+                ETtotalPax.setText("");
+                ETDiscount.setText("");
+                rbCash.setChecked(false);
+                rbPaynow.setChecked(false);
 
             }
         });
